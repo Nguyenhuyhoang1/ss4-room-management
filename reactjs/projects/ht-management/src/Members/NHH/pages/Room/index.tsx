773e648bc97b8ba645833834ed6f1c24a1df00af
HUYHOANG\nguye <nguyenhuyhoang765@gmail.com>
2023-04-27 08:43:58 +0700

    updatefile

--- reactjs/projects/ht-management/src/Members/NHH/pages/Room/index.tsx
+++ reactjs/projects/ht-management/src/Members/NHH/pages/Room/index.tsx
@@ -7,7 +7,7 @@ import Standars from "./Standars";
 import Double from "./Double";
 import King from "./King";
 import Siders from "./Siders";
-import Header1 from "./Header1";
+// import Header1 from "./Header1";
 export default function index() {
   return <div><Layout>
   <Sider style={{background: "#FFF", textAlign: "center", display: "inline-block"}}><Siders /> </Sider>   
